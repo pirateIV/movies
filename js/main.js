f88a7f5674eca059041070963d5471f1b302588e
ben64103 <benabolade@gmail.com>
2023-07-22 03:54:00 +0100

    mov. details stage

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 const api_key = "5e750355564957a2353604d8a9344e94";
-const api_url =
-  `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1"`;
+const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=2"`;
 
 // const api_url_genres = "https://api.themoviedb.org/3/genre/movie/list";
-const api_url_movies =
-  `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1"`;
+const api_url_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=2"`;
 const img_path = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API =
-  `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="war`;
-  // `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
+// `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
 
 const contentInfo = document.getElementById("content-info");
 const moviesHeader = document.getElementById("moviesHeader");
 
-// const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=20"`
-const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=20"`
+// const video_url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}`33333333333333
+// const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=2"`
+const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=2"`;
 async function getGenres() {
   const res = await fetch(
     "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + api_key
   );
-  const genreData = await res.json()
+  const genreData = await res.json();
 }
 
-let genres = []
+let genres = [];
 
 const genreMap = {
   12: "Adventure",
@@ -42,14 +40,14 @@ const genreMap = {
   10770: "TV Movie",
   53: "Thriller",
   10752: "War",
-  37: "Western"
-}
+  37: "Western",
+};
 
 getMovies(api_url);
-getGenres(api_url_genres)
-setInterval(() => {
-  getMovies(api_url, api_url_genres);
-}, 10000);
+getGenres(api_url_genres);
+// setInterval(() => {
+//   getMovies(api_url);
+// }, 10000);
 let genreName;
 
 async function getMovies(url) {
@@ -60,67 +58,84 @@ async function getMovies(url) {
   // console.log(data.results);
   updateHeader(data.results);
 }
-let mainData = []
-async function getGenres(api_url_genres){
-  const genreRes = await fetch(api_url_genres)
-  const genreData = await genreRes.json()
-  return genreData
-
+let mainData = [];
+async function getGenres(api_url_genres) {
+  const genreRes = await fetch(api_url_genres);
+  const genreData = await genreRes.json();
+  return genreData;
 }
 
 let currentIndex = 0;
-let random = Math.floor(Math.random() * 1)
+let random = Math.floor(Math.random() * 1);
 async function updateHeader(contents) {
   currentIndex++;
   if (currentIndex >= contents.length) {
     currentIndex = 0;
   }
+
+  // object to display movies
   contentInfo.innerHTML = "";
   const content = contents[currentIndex];
   const { id, title, vote_average, overview, poster_path, genre_ids } = content;
- const data  = await getGenres(api_url_genres)
-  console.log(genre_ids)
+  const data = await getGenres(api_url_genres);
+  console.log();
+  console.log(genre_ids);
 
-  // votes count
-  
-  // else if(vote_average <)
+  console.log(content);
+  // getMovieDetails(content)
 
   // Genre List
-  let result = []
-  let genreName = []
-  const res = genre_ids.forEach(element => {
-    data.genres.filter((dat)=>{
-      result.push(dat.id === element)
+  let result = [];
+  let genreName = [];
+  const res = genre_ids.forEach((element) => {
+    data.genres.filter((dat) => {
+      result.push(dat.id === element);
       if (dat.id === element) {
-        genreName.push(dat.name)
+        genreName.push(dat.name);
       }
-    })
+    });
   });
 
-  
+  // Update button
+  const button = document.createElement("button")
+  button.innerText = "Movie Details"
+  button.classList.add("px-5", "py-3", "rounded-pill", "shadow", "text-white", "bg-red")
+  button.addEventListener("click", ()=> {
+    // getMovieDetails(content)
+    console.log(content)
+    // console.log(content)
+  })
+
   moviesHeader.innerHTML = `
     <div class="container-mov content-container m-auto position-relative" id="content-info">
       <div class="content mt-5" style="width: 50%">
-      <div class="logo-header py-2 px-4" style="width: 200px">
-        <img class="img-fluid" src="img/logo.png" alt="" />
-      </div>
+        <div class="logo-header py-2 px-4" style="width: 200px">
+          <img class="img-fluid" src="img/logo.png" alt="" />
+        </div>
 
-      <div class="mov-info">
-        <div class="movie-info-header-text mt-3">
-          <h1 class="fw-200">${title}</h1>
-        </div>
-        <div class="movie-info nav align-items-center gap-3">
-          <small id="rating" class="rating badge bg-light ${getRatings(vote_average)}">${vote_average}</small>
-          <a class="genre text-white text-decoration-none">${genreName.join(", ")}</a>
-          <a class="mov-type text-white" id="mov-type"></a>
-        </div>
-        <div class="info mt-3">
-          <p class="mov-info text-white">${overview}</p>
+        <div class="mov-info">
+          <div class="movie-info-header-text mt-3">
+            <h1 class="fw-200">${title}</h1>
+          </div>
+          <div class="movie-info nav align-items-center gap-3">
+            <small id="rating" class="rating badge bg-light ${getRatings(
+              vote_average
+            )}">${vote_average}</small>
+            <a class="genre text-white text-decoration-none">${genreName.join(
+              ", "
+            )}</a>
+            <a class="mov-type text-white" id="mov-type"></a>
+          </div>
+          <div class="info mt-3">
+            <p class="mov-info text-white">${overview.slice(0, 240)}...</p>
+          </div>
+
+          <div>${button.outerHTML}</div>
         </div>
       </div>
     </div>
-    </div>
-    <img class="position-absolute br-30 shd" style="right: 60px; top: 90px" src="${img_path + poster_path
+    <img class="btn position-absolute br-30 shd" style="right: 60px; top: 90px" src="${
+      img_path + poster_path
     }" width="450" alt="" />
     `;
 
@@ -130,24 +145,24 @@ async function updateHeader(contents) {
     rgba(${0}, ${0}, ${0}, ${0.2}),
     rgba(${0}, ${0}, ${0}, ${0.8})),
     url(${img_path + poster_path})`;
-    
-    
+
+ 
 }
 
 function getRatings(vote) {
   if (vote >= 8) {
-    return 'text-success'
-  }
-  else if(vote >= 5) {
-    return 'text-warning'
-  }
-  else {
-    return 'text-danger'
+    return "text-success";
+  } else if (vote >= 5) {
+    return "text-warning";
+  } else {
+    return "text-danger";
   }
 }
 
+function getMovieDetails(movie) {
+  // console.log(movie);
 
-
-
-
-
+  // const movies = JSON.parse(movie);
+  console.log(movie);
+}
+// getMovieDetails()
