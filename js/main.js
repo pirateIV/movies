884ae39fb571d96eca62e1c0bf14a7326200abdb
ben64103 <benabolade@gmail.com>
2023-07-24 14:48:39 +0100

    ✨✨✨

--- js/main.js
+++ js/main.js
@@ -1,159 +1,159 @@
-  const api_key = "5e750355564957a2353604d8a9344e94";
-  const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
+const api_key = "5e750355564957a2353604d8a9344e94";
+const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
 
-  // const api_url_genres = "https://api.themoviedb.org/3/genre/movie/list";
-  const api_url_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1"`;
-  const img_path = "https://image.tmdb.org/t/p/w1280";
-  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
-  // `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
+// const api_url_genres = "https://api.themoviedb.org/3/genre/movie/list";
+const api_url_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1"`;
+const img_path = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
+// `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
 
-  const contentInfo = document.getElementById("content-info");
-  const moviesHeader = document.getElementById("moviesHeader");
+const contentInfo = document.getElementById("content-info");
+const moviesHeader = document.getElementById("moviesHeader");
 
-  // const video_url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}`33333333333333
-  // const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=2"`
+// const video_url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}`33333333333333
+// const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=2"`
 
-  // Get Genre list
-  const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=1"`;
+// Get Genre list
+const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=1"`;
 
-  // Popular movies list
-  const api_popular_list = `https://api.themoviedb.org/3/movie/popular?api_key=5e750355564957a2353604d8a9344e94&page=1  `;
-  async function getGenres() {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + api_key
-    );
-    const genreData = await res.json();
-  }
+// Popular movies list
+const api_popular_list = `https://api.themoviedb.org/3/movie/popular?api_key=5e750355564957a2353604d8a9344e94&page=1  `;
+async function getGenres() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + api_key
+  );
+  const genreData = await res.json();
+}
 
-  let genres = [];
+let genres = [];
 
-  const genreMap = {
-    12: "Adventure",
-    16: "Animation",
-    35: "Comedy",
-    80: "Crime",
-    99: "Documentary",
-    18: "Drama",
-    10751: "Family",
-    14: "Fantasy",
-    36: "History",
-    27: "Horror",
-    10402: "Music",
-    9648: "Mystery",
-    10749: "Romance",
-    878: "Science Fiction",
-    10770: "TV Movie",
-    53: "Thriller",
-    10752: "War",
-    37: "Western",
-  };
+const genreMap = {
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Science Fiction",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "War",
+  37: "Western",
+};
 
-  // getMovies(api_url);
+// getMovies(api_url);
 
-  updateHeader()
-  getPopular(api_popular_list);
-  getGenres(api_url_genres);
-  setInterval(() => {
-    getMovies(api_url);
-  }, 10000);
-  let genreName;
 
-  // fetch movies and specify minimum results
-  async function getMovies(url, minResults) {
-    try {
-      let totalResults = 0;
-      let currentPage = 1;
+getPopular(api_popular_list);
+getGenres(api_url_genres);
+// setInterval(() => {
+//   getMovies(api_url);
+// }, 10000);
+let genreName;
 
-      // add fetched movies to array
-      let fetchedMoviesArr = [];
+let mainData = [];
 
-      while (fetchedMoviesArr.length < minResults) {
-        const response = await fetch(`${url}&page=${currentPage}`);
-        const data = await response.json();
+// Get Genres
+async function getGenres(api_url_genres) {
+  const genreRes = await fetch(api_url_genres);
+  const genreData = await genreRes.json();
+  return genreData;
+}
 
-        fetchedMoviesArr = fetchedMoviesArr.concat(data.results);
-        totalResults = data.total_results;
-        currentPage++;
+let currentIndex = 0;
+let random = Math.floor(Math.random() * 1);
 
-        if (currentPage > data.total_pages) {
-          // stop fetching if there are no more pages 
-          break;
-        }
+async function getMovies(url, minResults) {
+  try {
+    let totalResults = 0;
+    let currentPage = 1;
+
+    // add fetched movies to array
+    let fetchedMoviesArr = [];
+
+    while (fetchedMoviesArr.length < minResults) {
+      const response = await fetch(`${url}&page=${currentPage}`);
+      const data = await response.json();
+
+      fetchedMoviesArr = fetchedMoviesArr.concat(data.results);
+      totalResults = data.total_results;
+      currentPage++;
+
+      if (currentPage > data.total_pages) {
+        // stop fetching if there are no more pages
+        break;
       }
-
-      return fetchedMoviesArr.slice(20, minResults);
-    } catch (error) {
-      console.log(error.message);
     }
+
+    return fetchedMoviesArr.slice(25, minResults);
+  } catch (error) {
+    console.log(error.message);
   }
-  let mainData = [];
+}
 
-  // Get Genres
-  async function getGenres(api_url_genres) {
-    const genreRes = await fetch(api_url_genres);
-    const genreData = await genreRes.json();
-    return genreData;
+// function for header / first page
+async function updateHeader() {
+  const minMoviesToFetch = 100;
+  const movies = await getMovies(api_url, minMoviesToFetch);
+
+  currentIndex++;
+  if (currentIndex >= movies.length) {
+    currentIndex = 22;
   }
+  try {
+    // object to display movies
+    contentInfo.innerHTML = "";
 
-  let currentIndex = 0;
-  let random = Math.floor(Math.random() * 1);
+    const movie = movies[currentIndex];
+    const { id, title, vote_average, overview, poster_path, genre_ids } = movie;
+    const data = await getGenres(api_url_genres);
 
-  async function updateHeader() {
-    try {
-      const minMoviesToFetch = 100;
-      const movies = await getMovies(api_url, minMoviesToFetch);
-
-      currentIndex++;
-      if (currentIndex >= movies.length) {
-        currentIndex = 22;
-      }
-
-      // object to display movies
-      contentInfo.innerHTML = "";
-      
-      const movie = movies[currentIndex];
-      const { id, title, vote_average, overview, poster_path, genre_ids } = movie;
-      const data = await getGenres(api_url_genres);
-
-      // Genre List
-      let result = [];
-      let genreName = [];
-      const res = genre_ids.forEach((element) => {
-        data.genres.filter((dat) => {
-          result.push(dat.id === element);
-          if (dat.id === element) {
-            genreName.push(dat.name);
-          }
-        });
+    // Genre List
+    let result = [];
+    let genreName = [];
+    const res = genre_ids.forEach((element) => {
+      data.genres.filter((dat) => {
+        result.push(dat.id === element);
+        if (dat.id === element) {
+          genreName.push(dat.name);
+        }
       });
+    });
 
-      // Update button
-      const button = document.createElement("button");
-      button.innerText = "Movie Details";
-      button.classList.add(
-        "px-5",
-        "py-3",
-        "rounded-pill",
-        "shadow",
-        "text-white",
-        "bg-red"
-      );
-      button.addEventListener("click", () => {
-        // getMovieDetails(content)
-        console.log(content);
-        // console.log(content)
-      });
+    // Update button
+    const button = document.createElement("button");
+    button.innerText = "Movie Details";
+    button.classList.add(
+      "px-5",
+      "py-3",
+      "rounded-pill",
+      "shadow",
+      "text-white",
+      "bg-red"
+    );
+    button.addEventListener("click", () => {
+      // getMovieDetails(content)
+      console.log(content);
+      // console.log(content)
+    });
 
-      moviesHeader.innerHTML = `
-      <div class="container-mov content-container m-auto position-relative" id="content-info">
-        <div class="content mt-5">
-          <div class="logo-header py-2 px-4 " style="width: 200px">
+    moviesHeader.innerHTML = `
+      <div class="container-mov content-container m-auto position-relative" style="height: 70vh" id="content-info">
+        <div class="content mt-5 d-flex flex-column " style="height:inherit">
+          <div class="logo-header py-2 px-4" style="width: 200px">
             <img class="img-fluid" src="img/logo.png" alt="" />
           </div>
 
           <div class="mov-info">
-            <div class="movie-info-header-text mt-3">
-              <h1 class="fw-900">${title}</h1>
+            <div class="movie-info-header-text">
+              <h1 class="fw-900">${title.slice(0, 36)}</h1>
             </div>
             <div class="movie-info nav align-items-center gap-3">
               <small id="rating" class="rating badge bg-light ${getRatings(
@@ -177,71 +177,71 @@
       }" width="450" alt="" />
       `;
 
-      header.style.background = `
+    header.style.background = `
       linear-gradient(to right,
       rgba(${0}, ${0}, ${0}, ${0.9}) 30%,
       rgba(${0}, ${0}, ${0}, ${0.2}),
       rgba(${0}, ${0}, ${0}, ${0.8})),
       url(${img_path + poster_path})`;
-    } catch (error) {
-      // console.log(error);
-    }
+  } catch (error) {
+    // console.log(error);
   }
+}
 
-  function getRatings(vote) {
-    if (vote >= 8) {
-      return "text-success";
-    } else if (vote >= 5) {
-      return "text-warning";
-    } else {
-      return "text-danger";
-    }
+function getRatings(vote) {
+  if (vote >= 8) {
+    return "text-success";
+  } else if (vote >= 5) {
+    return "text-warning";
+  } else {
+    return "text-danger";
   }
+}
 
-  // function getMovieDetails(movie) {
-  //   // console.log(movie);
+// function getMovieDetails(movie) {
+//   // console.log(movie);
 
-  //   // const movies = JSON.parse(movie);
-  //   console.log(movie);
-  // }
-  // getMovieDetails()
+//   // const movies = JSON.parse(movie);
+//   console.log(movie);
+// }
+// getMovieDetails()
 
-  // async function getPopular(list_content) {
-  //   const res = await fetch(list_content);
-  //   const data = await res.json();
+// async function getPopular(list_content) {
+//   const res = await fetch(list_content);
+//   const data = await res.json();
 
-  //   topRatedMovies.innerHTML = "";
+//   topRatedMovies.innerHTML = "";
 
-  //   data.results.forEach((movList) => {
-  //     const { id, poster_path, overview, vote_average } = movList;
+//   data.results.forEach((movList) => {
+//     const { id, poster_path, overview, vote_average } = movList;
 
-  //   });
+//   });
 
-  // }
+// }
 
-  // function displayCards(startIndex, count) {
-  //   for (let i = startIndex; i <= count; i++) {
-  //     topRatedMovies.innerHTML += `
-  //       <div class="list me-5" id="list">
-  //         <img src="${img_path + poster_path}" alt=""
-  //       </div>
+// function displayCards(startIndex, count) {
+//   for (let i = startIndex; i <= count; i++) {
+//     topRatedMovies.innerHTML += `
+//       <div class="list me-5" id="list">
+//         <img src="${img_path + poster_path}" alt=""
+//       </div>
 
-  //     `;
-  //   }
-  // }
-  // displayCards(0, 5)
+//     `;
+//   }
+// }
+// displayCards(0, 5)
 
-  async function getPopular(api_popular_list) {
-    const res = await fetch(api_popular_list);
-    const data = await res.json();
+async function getPopular(api_popular_list) {
+  const res = await fetch(api_popular_list);
+  const data = await res.json();
 
-    console.log(data);
-    let listData = data.results;
-  }
-  function displayCards(startIndex, count) {
-    for (let i = startIndex; i <= count; i++) {}
-  }
+  console.log(data);
+  let listData = data.results;
+}
+function displayCards(startIndex, count) {
+  for (let i = startIndex; i <= count; i++) {}
+}
 
-  displayCards(0, 5);
+displayCards(0, 5);
 
-  updateHeader()
+updateHeader();
