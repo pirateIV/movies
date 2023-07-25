8c80bfd0c771df24213062308d73bc1b67acc962
ben64103 <benabolade@gmail.com>
2023-07-25 11:39:09 +0100

    ✨✨✨ order updated

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-const api_key = "5e750355564957a2353604d8a9344e94";
-const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
-
-// const api_url_genres = "https://api.themoviedb.org/3/genre/movie/list";
-const api_url_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1"`;
-const img_path = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
-// `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
-
-const contentInfo = document.getElementById("content-info");
-const moviesHeader = document.getElementById("moviesHeader");
-
-// const video_url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}`33333333333333
-// const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=2"`
-
-// Get Genre list
-const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=1"`;
-
-// Popular movies list
-const api_popular_list = `https://api.themoviedb.org/3/discover/movie?api_key=5e750355564957a2353604d8a9344e94&sort_by=popularity.desc&page=1  `;
-
-// airing today movies
-const api_air_today = `https://api.themoviedb.org/3/tv/airing_today?api_key=${api_key}`;
-
-async function getGenres() {
-  const res = await fetch(
-    "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + api_key
-  );
-  const genreData = await res.json();
-}
-
 let genres = [];
 
 const genreMap = {
@@ -211,12 +180,11 @@ function checkLength(text) {
   }
 }
 
-
 async function getPopular() {
   let popularMoviesLength = 100;
   const movList = document.querySelector(".mov-list");
   movList.style.gridTemplateColumns = `repeat(${popularMoviesLength}, ${1}fr)`;
-  let movieContents = await getMovies(api_popular_list, popularMoviesLength)
+  let movieContents = await getMovies(api_popular_list, popularMoviesLength);
   try {
     // console.log(movieContents);
 
@@ -258,8 +226,7 @@ async function getPopular() {
       // console.log(topRatedMovies.length)
       const lists = document.querySelectorAll("#list");
       lists.forEach((list, index) => {
-        
-        list.addEventListener('click', (e) => getMovieDetails(index))
+        list.addEventListener("click", (e) => getMovieDetails(index));
       });
     }
   } catch (error) {
@@ -284,10 +251,4 @@ function votesPercentage(percentage) {
   const borderLength = (circumference * percentage) / 100;
 
   return `${borderLength} ${circumference - borderLength}`;
-
-  // const percentageOfBorder = percentage;
-}
-
-{
-  /* <div class="vote nav text-white justify-content-center align-items-center bottom-0 rounded-circle end-0 ${updateVotesAverage(vote_average * 10)}">${vote_average * 10}<sup>%</sup></div> */
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+const api_key = "5e750355564957a2353604d8a9344e94";
+const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
 
-// get Movie Details
+// const api_url_genres = "https://api.themoviedb.org/3/genre/movie/list";
+const api_url_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1"`;
+const img_path = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
+// `https://api.themoviedb.org/3/search/movie?api_key=5e750355564957a2353604d8a9344e94&query="war`;
 
+const contentInfo = document.getElementById("content-info");
+const moviesHeader = document.getElementById("moviesHeader");
 
+// const video_url = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}`33333333333333
+// const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key="${api_key}&page=2"`
 
+// Get Genre list
+const api_url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=5e750355564957a2353604d8a9344e94&page=1"`;
+
+// Popular movies list
+const api_popular_list = `https://api.themoviedb.org/3/discover/movie?api_key=5e750355564957a2353604d8a9344e94&sort_by=popularity.desc&page=1  `;
+
+// airing today movies
+const api_air_today = `https://api.themoviedb.org/3/tv/airing_today?api_key=${api_key}`;
+
+async function getGenres() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + api_key
+  );
+  const genreData = await res.json();
+}
