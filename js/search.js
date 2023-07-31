1ee78e51404e9f40243b607be15e7cb138a6de72
ben64103 <benabolade@gmail.com>
2023-07-31 15:30:34 +0100

    ✨✨✨

--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
-// api key reference = 5e750355564957a2353604d8a9344e94
+const api_key = "5e750355564957a2353604d8a9344e94";
+const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
 const search_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
 
 const form = document.getElementById("search-form");
