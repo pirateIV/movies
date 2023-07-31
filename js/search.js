3b087423ad0957bd2a7c7d5851d8cda009b6a499
ben64103 <benabolade@gmail.com>
2023-07-31 15:41:20 +0100

    ✨✨✨

--- js/search.js
+++ js/search.js
@@ -5,13 +5,24 @@ const search_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}
 const form = document.getElementById("search-form");
 const search = document.getElementById('search')
 
+searchMovies(api_url)
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
   const searchTerm = search.value
+  if(searchTerm && searchTerm !== '') {
+    searchMovies(search_api + searchTerm)
+    search.value = ''
+  }
   console.log(searchTerm)
 
 });
 
-let searchWord;
-function searchMovies(movie) {}
+async function searchMovies(url) {
+  const movieRes = await fetch(url)
+  const data = await movieRes.json()
+
+  console.log(data.results)
+  let searchResArr = []
+  searchResArr.push(data.results)
+}
