ee44f24c374f4abef2c978d3119246eda843cae9
ben64103 <benabolade@gmail.com>
2023-07-31 14:56:29 +0100

    ...

--- js/search.js
+++ js/search.js
@@ -1,8 +1,7 @@
 // api key reference = 5e750355564957a2353604d8a9344e94
 const search_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="`;
 
+const form = document.getElementById('search-form')
+
 let searchWord;
-function searchMovies(movie) {
-  .
-  
-}
+function searchMovies(movie) {}
