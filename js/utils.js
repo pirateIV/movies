deb40dc0d886307e3c262b1efd390185795870aa
ben64103 <benabolade@gmail.com>
2023-08-01 13:49:39 +0100

    ✨✨✨

--- js/utils.js
+++ js/utils.js
@@ -3,15 +3,12 @@ const navUl = document.querySelector("#nav-links")
 const navLinks = document.querySelectorAll("#nav-links li")
 const navLinksAnchor = document.querySelectorAll("#nav-links li a")
 
-navToggle.addEventListener('click', (e) => {
-  // navUl.style.height = 0
-  navLinks.forEach((link, idx) => {
-    link.style.height = 0;
-    console.log(link.getBoundingClientRect().height)
-  })
-  navLinksAnchor.forEach((anchor, idx) => {
-    console.log(anchor.getBoundingClientRect().height)
-  })
 
-  e.preventDefault()
+navToggle.addEventListener('click', (e) => {
+  console.log(e.target)
+  // const navLinkHeight = navLinks.getBoundingClientRect().height
+  navLinks.forEach((link) => {
+    const linkHeight = link.getBoundingClientRect().height
+    console.log(linkHeight)
+  })
 })
