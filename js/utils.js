48165504dfd036c5e962934d763cb583c139905f
Harmedino <adamsahmed191@gmail.com>
2023-08-06 10:18:38 +0100

    working navbar added

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,16 @@
-const navToggle = document.querySelector("#nav-toggle")
-const navLinks = document.querySelector(".nav-links")
+const navToggle = document.querySelector("#nav-toggle");
+const navLinks = document.querySelector(".nav-links");
 
 navLinks.addEventListener("click", () => {
-    displayLinks()
-    navLinks.classList.toggle("show")
-    console.log(123)
-})
+  //   alert();
+  displayLinks();
+  //   navLinks.classList.toggle("show");
+  console.log(123);
+});
 
-function displayLinks() {
-}
+function displayLinks() {}
+
+navToggle.addEventListener("click", () => {
+  //   alert();
+  navLinks.classList.toggle("show");
+});
