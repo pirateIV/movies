da8df7a93cdf00ba978ee9aaa031c2440847b560
ben64103 <benabolade@gmail.com>
2023-08-06 18:47:18 +0100

    navbar fixed by @Harmedino

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
 const navToggle = document.querySelector("#nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-navLinks.addEventListener("click", () => {
-  displayLinks();
-  navLinks.classList.toggle("show");
-  console.log(123);
-});
+// navLinks.addEventListener("click", () => {
+//   displayLinks();
+//   navLinks.classList.toggle("show");
+//   console.log(123);
+// });
 
 function displayLinks() {}
 
