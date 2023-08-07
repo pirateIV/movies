325811a9e760474480495c95d2680d9575afbf6d
ben64103 <benabolade@gmail.com>
2023-08-07 12:54:54 +0100

    ...update

--- js/movies.js
+++ js/movies.js
@@ -2,7 +2,7 @@
 // const api_credits = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`;
 // https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
 
-getMovies(api_url, minResults)
+getMovies(api_url, minResults);
 
 async function getMovies(url, minResults) {
   try {
@@ -28,19 +28,31 @@ async function getMovies(url, minResults) {
 
     // console.log(fetchedMoviesArr)
 
-    // return fetchedMoviesArr.slice(130, minResults);\
-    return fetchedMoviesArr
+    // return fetchedMoviesArr.slice(130, minResults);
+    return fetchedMoviesArr;
   } catch (error) {
     console.log(error.message);
   }
 }
 
-async function getMovieResults(){
-let minResults = 300;
+async function getMovieResults() {
+  let minResults = 300;
 
-  const movies = await getMovies(api_url, minResults)
+  const movies = await getMovies(api_url, minResults);
+  const {
+    title,
+    vote_average,
+    overview,
+    poster_path,
+    backdrop_path,
+    genre_ids,
+  } = movies;
 }
 
 function getSimilar(movieId) {
-  const api_similar = `https://api.themoviedb.org/3/movie/${movieId}/similar`
+  const api_similar = `https://api.themoviedb.org/3/movie/${movieId}/similar`;
 }
+
+function getMovieFullDetails() {}
+
+function getCredits() {}
