6004b44c52c4869997606990e38244d8db3b14cb
ben64103 <benabolade@gmail.com>
2023-08-07 02:37:36 +0100

    ...

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,6 @@
 // Movie Credits
 
 
-// https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94
-// https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
+const api_mov_details = `https://api.themoviedb.org/3/movie/{movie_id}`
+const api_credits = `https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`
+// `https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94`
