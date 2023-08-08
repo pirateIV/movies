9a1da68711ecaa2850684d8d6cbd05fff5fa8204
ben64103 <benabolade@gmail.com>
2023-08-08 03:15:09 +0100

    full mov details

--- js/movies.js
+++ js/movies.js
@@ -1,74 +1,24 @@
-// Movie Credits
+// // Movie Credits
 // const api_credits = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`;
-// https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
+// // https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
 
-getMovieResults();
-
-const api_mov_details = `https://api.themoviedb.org/3/movie/{movie_id}`
-// const api_key = "5e750355564957a2353604d8a9344e94";
 // const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
-const api_credits = `https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`
-// `https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94`
-const minResults = 300;
-getMovies(api_url, minResults)
-// script js - line 230
-async function getMovies(url, minResults) {
-  try {
-    let totalResults = 0;
-    let currentPage = 1;
+const api_mov_details = `
+https://api.themoviedb.org/3/movie/{movie_id}`
 
-    // add fetched movies to array
-    let fetchedMoviesArr = [];
+getMovies(api_url)
+async function getMovies(url){
+  const resp = await fetch(url)
+  const data = await resp.json()
 
-    while (fetchedMoviesArr.length < minResults) {
-      const response = await fetch(`${url}&page=${currentPage}`);
-      const data = await response.json();
-
-      fetchedMoviesArr = fetchedMoviesArr.concat(data.results);
-      totalResults = data.total_results;
-      currentPage++;
-
-      if (currentPage > data.total_pages) {
-        // stop fetching if there are no more pages
-        break;
-      }
-    }
-
-    // console.log(fetchedMoviesArr)
-
-    // return fetchedMoviesArr.slice(130, minResults);
-    // console.log(fetchedMoviesArr)
-  } catch (error) {
-    console.log(error.message);
-  }
+  console.log(data.results)
+  getMovieDetails(data.results[0].id)
 }
 
-async function getMovieResults() {
-  let minResults = 300;
+async function getMovieDetails(mov_detail_id) {
+  const resp = await fetch(`
+  https://api.themoviedb.org/3/movie/${mov_detail_id}?api_key=${api_key}`)
+  const data = await resp.json()
 
-  const movies = await getMovies(api_url, minResults);
-  const {
-    id,
-    title,
-    vote_average,
-    overview,
-    poster_path,
-    backdrop_path,
-    genre_ids,
-  } = movies;
-
-  const similar = await getSimilar(id)
-  console.log(similar)
+  console.log(data)
 }
-
-async function getSimilar(movieId) {
-  const api_similar = `https://api.themoviedb.org/3/movie/${movieId}/similar`;
-  // const resSimilar = await fetch(api_similar)
-  // const dataSimilar = await resSimilar.json()
-
-  // console.log(dataSimilar.results)
-}
-
-function getMovieFullDetails() {}
-
-function getCredits() {}
