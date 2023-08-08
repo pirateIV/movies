0762771813219536d7f3e2b175956e3067b53dbf
ben64103 <benabolade@gmail.com>
2023-08-08 12:15:00 +0100

    ...movie header update

--- js/movies.js
+++ js/movies.js
@@ -2,80 +2,95 @@
 // const api_credits = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`;
 // // https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
 
-
 const api_key = "5e750355564957a2353604d8a9344e94";
 const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
 const api_mov_details = `
-https://api.themoviedb.org/3/movie/{movie_id}`
+https://api.themoviedb.org/3/movie/{movie_id}`;
 const img_path = "https://image.tmdb.org/t/p/w1280";
 
-
-getMovies(api_url)
-async function getMovies(url){
-  const resp = await fetch(url)
-  const data = await resp.json()
+let currentIndex  = 0
+getMovies(api_url);
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const data = await resp.json();
 
   // console.log(data.results)
-  getMovieDetails(data.results[0].id)
-  getMovieCredits(data.results[0].id)
-  getSimilarMovies(data.results[0].id)
+  getMovieDetails(data.results[0].id);
+  getMovieCredits(data.results[0].id);
+  getSimilarMovies(data.results[0].id);
 
-  data.results.forEach((objectData, index) => {
-    console.log(objectData)
+  const movies = data.results
+  const { id, title, poster_path, backdrop_path, vote_average, overview } = movies[currentIndex]
 
-    mainSection.style.background =  `url(${img_path + data.results[0].backdrop_path})`
+  let movie = movies[currentIndex]
+
+  movies.forEach((objectData, index) => {
+    console.log(objectData);
+
+    mainSection.style.background = ` linear-gradient(black, rgba(0,0,0,0.4)), url(${
+      img_path + objectData.backdrop_path
+    })`;
     mainSection.innerHTML = `
-      <div class="w-75 m-0 ">
-        <h1 class="title text-white">${ data.results[0].title}</h1>
-        <div class="ratings">
-          <div class="rate-count bg-success d-flex">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-          ${ data.results[0].vote_average } / 10
+    <div class="container-section">
+      <h1 class="title text-white">${objectData.title}</h1>
+      <div class="ratings">
+        <div class="rate-count d-flex">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
         </div>
-
-        <div class="story-line text-light">${ data.results[0].overview}</div>
-        <button class="btn  btn-warning  py-2 rounded-2"><i class="fas fa-play text-danger"></i> Watch Trailer</button>
+        ${objectData.vote_average} / 10
       </div>
-    `
+  
+      <div class="story-line text-light">${objectData.overview}</div>
+      <button><i class="fas fa-play"></i> Watch Trailer</button>
+    </div>
+  `
+  
+  ;
+  
+  // Call the updateStars function to update the stars based on the rating
+  updateStars(rating);
+  
+
+    // Example usage: Update stars with a rating of 5
+    updateStars(data.results[0].overview);
   });
 }
 
 async function getMovieDetails(mov_detail_id) {
   const resp = await fetch(`
-  https://api.themoviedb.org/3/movie/${mov_detail_id}?api_key=${api_key}`)
-  const data = await resp.json()
+  https://api.themoviedb.org/3/movie/${mov_detail_id}?api_key=${api_key}`);
+  const data = await resp.json();
 
-  console.log(data)
+  console.log(data);
 }
 
 async function getSimilarMovies(similar_id) {
   const resp = await fetch(
     `https://api.themoviedb.org/3/movie/${similar_id}/similar?api_key=${api_key}`
-  )
-  const data = await resp.json()
+  );
+  const data = await resp.json();
 
-  console.log(data)
+  console.log(data);
 }
 
 async function getMovieCredits(credits_id) {
-  const creditsResponse = await fetch (
+  const creditsResponse = await fetch(
     `https://api.themoviedb.org/3/movie/${credits_id}/credits?api_key=${api_key}`
-  )
-  const creditsData = await creditsResponse.json()
+  );
+  const creditsData = await creditsResponse.json();
 
-  console.log(creditsData)
+  console.log(creditsData);
 }
 
 async function getPersonMovieCredits(person_id) {
-  const personCreditRes = await fetch (
-   `https://api.themoviedb.org/3/person/${person_id}/movie_credits?api_key=${api_key}`
-  )
-  const personData = await personCreditRes.json()
+  const personCreditRes = await fetch(
+    `https://api.themoviedb.org/3/person/${person_id}/movie_credits?api_key=${api_key}`
+  );
+  const personData = await personCreditRes.json();
 
-  console.log(personData)
+  console.log(personData);
 }
