3a0d47e1226c3133bd0c54273b0c2d3422f996e2
ben64103 <benabolade@gmail.com>
2023-08-16 14:27:42 -0700

    ...

--- js/movies.js
+++ js/movies.js
@@ -2,6 +2,7 @@
 // const api_credits = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=5e750355564957a2353604d8a9344e94`;
 // // https://api.themoviedb.org/3/movie/298618/credits?api_key=5e750355564957a2353604d8a9344e94
 
+// ----- Enter search query----------------------------// 
 const api_key = "5e750355564957a2353604d8a9344e94";
 const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`;
 const api_mov_details = `
@@ -244,8 +245,8 @@ async function getMovieDetails(mov_detail_id) {
                         cst.original_name
                       }">`}
                     </div>
-                    <h6>${cst.original_name}</h6>
-                    <small class="text-white d-block">${cst.department}</small>
+                    <h6 class="text-white">${cst.original_name}</h6>
+                    <small class="opacity-50 d-block">${cst.department}</small>
                 </div>`
             )
             .join(" ")}
