b04393b5947243935b28b22db3621b40efa8bbbc
ben64103 <benabolade@gmail.com>
2023-08-17 11:03:17 -0700

    created pages js

---
+++ js/pages.js
@@ -0,0 +1,38 @@
+const api_key = "5e750355564957a2353604d8a9344e94";
+const ID = JSON.parse(localStorage.getItem("cast-id"));
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function d(id) {
+  const url = `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${api_key}`;
+  const data = await fetchData(url);
+  console.log("Similar Movies:", data);
+}
+
+async function getPersonImages(id) {
+  const url = `https://api.themoviedb.org/3/person/${id}/images?api_key=${api_key}`;
+  const data = await fetchData(url);
+  console.log("Person Images:", data);
+}
+
+async function getPersonDetails(details_id) {
+  const url = `https://api.themoviedb.org/3/person/${details_id}?api_key=${api_key}`;
+  const data = await fetchData(url);
+  console.log("Person Details:", data);
+}
+
+async function main() {
+  try {
+    await getPersonImages(ID);
+    await getPersonDetails(ID);
+    await getPersonMovies(ID);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+main();
