04791d5245254a0dd4c175d7f98c583dbb26530f
ben64103 <benabolade@gmail.com>
2023-09-04 10:24:24 -0700

    updating pages

--- js/pages.js
+++ js/pages.js
@@ -1,22 +1,22 @@
-const api_key = "5e750355564957a2353604d8a9344e94";
-const img_path = "https://image.tmdb.org/t/p/w1280";
+const api_key = '5e750355564957a2353604d8a9344e94';
+const img_path = 'https://image.tmdb.org/t/p/w1280';
 
 const imdb_href = `https://www.imdb.com/name/`;
 
-const ID = JSON.parse(localStorage.getItem("person-id"));
+const ID = JSON.parse(localStorage.getItem('person-id'));
 
-const castName = document.querySelector(".cast-name");
+const castName = document.querySelector('.cast-name');
 
-const castInfo = document.getElementById("castInfo");
-const birthdate = document.getElementById("birthdate");
-const castHeader = document.getElementById("castHeader");
-const profileImg = document.getElementById("profileImg");
-const imdbPage = document.getElementById("imdbPage");
-const tabOne = document.getElementById("tab1");
-const tabTwo = document.getElementById("tab2");
-const tabThree = document.getElementById("tab3");
+const castInfo = document.getElementById('castInfo');
+const birthdate = document.getElementById('birthdate');
+const castHeader = document.getElementById('castHeader');
+const profileImg = document.getElementById('profileImg');
+const imdbPage = document.getElementById('imdbPage');
+const tabOne = document.getElementById('tab1');
+const tabTwo = document.getElementById('tab2');
+const tabThree = document.getElementById('tab3');
 
-const creditContainer = document.getElementById("creditContainer")
+const creditContainer = document.getElementById('creditContainer');
 // const castInfo = document.querySelector("#castInfo");
 
 async function fetchData(url) {
@@ -62,15 +62,15 @@ async function getCastInfo(id) {
     known_for_department,
   } = castDetailsDATA;
 
-  const castBio = biography.split(". ");
+  const castBio = biography.split('. ');
 
   castName.innerHTML = name;
   birthdate.innerHTML = birthday;
   // imbdPage.href = `${imbd_href + imdb_id}`
-  imdbPage.setAttribute("href", `${imdb_href}${imdb_id}`);
+  imdbPage.setAttribute('href', `${imdb_href}${imdb_id}`);
   castInfo.innerHTML = castBio
     .map((item) => (item = `<p>${item}.</p>`))
-    .join(" ");
+    .join(' ');
   birth.innerHTML = place_of_birth;
   job.innerHTML = known_for_department;
   profileImg.src = img_path + castImgsDATA.profiles[0].file_path;
@@ -78,31 +78,31 @@ async function getCastInfo(id) {
 
 // tabs
 
-const tabs = document.querySelectorAll(".tab");
-const tabContent = document.querySelectorAll(".tab-content");
+const tabs = document.querySelectorAll('.tab');
+const tabContent = document.querySelectorAll('.tab-content');
 
 tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const tabId = tab.getAttribute("data-tab");
+  tab.addEventListener('click', (e) => {
+    const tabId = tab.getAttribute('data-tab');
 
     tabContent.forEach((content) => {
-      content.classList.add("d-none");
+      content.classList.add('d-none');
     });
 
     tabs.forEach((otherTab) => {
-      otherTab.classList.remove("border-bottom");
+      otherTab.classList.remove('border-bottom');
     });
 
     tabs.forEach((otherTab) => {
-      const otherTabId = otherTab.getAttribute("data-tab");
+      const otherTabId = otherTab.getAttribute('data-tab');
       if (otherTab !== tab) {
-        document.getElementById(otherTabId).classList.add("d-none");
+        document.getElementById(otherTabId).classList.add('d-none');
       }
     });
 
-    document.getElementById(tabId).classList.add("d-flex");
-    document.getElementById(tabId).classList.remove("d-none");
-    tab.classList.add("border-bottom");
+    document.getElementById(tabId).classList.add('d-flex');
+    document.getElementById(tabId).classList.remove('d-none');
+    tab.classList.add('border-bottom');
   });
 });
 
@@ -131,33 +131,64 @@ async function getCombCredits(combCreditsDATA) {
   console.log(combCreditsDATA);
   const cast = combCreditsDATA.cast;
   const crew = combCreditsDATA.crew;
-  // combCreditsDATA =
+  // Clear the creditContainer before adding new items
+  creditContainer.innerHTML = '';
+
   let item = [...cast, ...crew];
   item.forEach((item, index) => {
-    creditContainer.innerHTML += 
-    `
-    <a class="credit credit-active text-decoration-none text-white ff-roboto position-relative">
-      <span class="credit-title text- fw-bold class"postion-relative z-2=3">${item.title ? item.title : item.original_name}</span>
+    const creditElement = document.createElement('a');
+    creditElement.classList.add(
+      'credit',
+      'text-decoration-none',
+      'text-white',
+      'ff-roboto',
+      'position-relative'
+    );
 
-      <button class="border-1 rounded-circle position-absolute end-0 me-3">
-        <i class="fa-solid fa-chevron-down"></i>
-        <i class="fa-solid fa-xmark"></i>
-      </button>
+    const creditTitle = document.createElement('span');
+    creditTitle.classList.add(
+      'credit-title',
+      'text-',
+      'fw-bold',
+      'position-relative',
+      'z-2=3'
+    );
+    creditTitle.textContent = item.title ? item.title : item.original_name;
 
-      <p class="postion-relative z-3 overview">${item.overview}</p>
-    </a>
-   `
+    const toggleBtn = document.createElement('button');
+    toggleBtn.id = 'toggleBtn';
+    toggleBtn.classList.add(
+      'border-1',
+      'rounded-circle',
+      'position-absolute',
+      'end-0',
+      'me-3'
+    );
+
+    const chevronIcon = document.createElement('i');
+    chevronIcon.classList.add('fa-solid', 'fa-chevron-down');
+
+    const xmarkIcon = document.createElement('i');
+    xmarkIcon.classList.add('fa-solid', 'fa-xmark');
+
+    toggleBtn.appendChild(chevronIcon);
+    toggleBtn.appendChild(xmarkIcon);
+
+    const overview = document.createElement('p');
+    overview.classList.add('position-relative', 'z-3', 'overview');
+    overview.textContent = item.overview;
+
+    creditElement.appendChild(creditTitle);
+    creditElement.appendChild(toggleBtn);
+    creditElement.appendChild(overview);
+
+    creditContainer.appendChild(creditElement);
+
+    // Add click event listener to each toggleBtn
+    toggleBtn.addEventListener('click', () => {
+      creditElement.classList.toggle('credit-active');
+
+      console.log(123);
+    });
   });
 }
-
-{/* <div>
-        <img
-          src="${img_path + item.backdrop_path}"
-          alt="${item.title ? item.title : item.original_name}">
-        <a class="d-block p-3 text-decoration-none text-white rounded-3 bg-222">${
-          item.title ? item.title : item.original_name
-        }</a>
-      </div> */}
-
-
-      
