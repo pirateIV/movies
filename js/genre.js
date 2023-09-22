125688933ea9e1cb3afdd33c053f8d25f6c3c321
ben64103 <benabolade@gmail.com>
2023-09-22 09:36:14 +0100

    fetch mov. id onload

--- js/genre.js
+++ js/genre.js
@@ -1,8 +1,8 @@
 // let genre_id = 28;
 
 // const genreOption = `https://api.themoviedb.org/3/discover/movie?api_key=###&with_genres=`
-const api_key = "5e750355564957a2353604d8a9344e94";
-const img_path = "https://image.tmdb.org/t/p/w1280";
+// const api_key = "5e750355564957a2353604d8a9344e94";
+// const img_path = "https://image.tmdb.org/t/p/w1280";
 const genreListContainer = document.getElementById("genre-list");
 const movieListContainer = document.getElementById("movie-list");
 const resultsPerPage = 20; // Number of results per page
@@ -49,11 +49,7 @@ const moviesArr = [
 
 moviesArr.forEach(item => {
   movieListContainer.innerHTML += `
-    <div>
-      <article>
-      </article>
-      <mov-title class="text-white w-100">The Guardians of the Galaxy - Holiday special</mov-title>
-    </div>
+    
   `
 })
 
@@ -70,10 +66,11 @@ function fetchMoviesByGenre(genreId, page) {
       // Display movies
       movies.forEach(movie => {
         movieListContainer.innerHTML += `
-          <div>
-            <h4>${movie.title}</h4>
-            <img width="300" src="${img_path + movie.poster_path}" loading="lazy"> 
-          </div>
+        <div>
+        <article>
+        </article>
+        <mov-title class="text-white w-100">${movie.title}</mov-title>
+      </div>
         `
       });
 
@@ -119,3 +116,5 @@ function fetchMovieGenres() {
 window.addEventListener("load", () => {
   fetchMovieGenres();
 });
+
+fetchMoviesByGenre(18, 1)
