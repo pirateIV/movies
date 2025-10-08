02fa6211f35dae56076acf59c90e9ad15d60f797
Benjamin <benabolade@gmail.com>
2025-10-08 03:22:06 +0100

    (chore): fix person pages

--- src/pages/person/index.jsx
+++ src/pages/person/index.jsx
@@ -3,6 +3,9 @@ import { getPerson } from "@/services/tmdb";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const buildURL = (imagePath, size) =>
+  `${imgBaseURL}/f_webp&s_${size}/tmdb/${imagePath}`;
+
 const Person = () => {
   const params = useParams();
   const [person, setPerson] = useState(null);
@@ -17,21 +20,35 @@ const Person = () => {
   }, [params]);
 
   return (
-    <div className="max-w-300 mx-auto order-first lg:order-last">
-      <div className="grid grid-cols-[auto_1fr] p-4">
-        {/* <div> */}
-        <img
-          src={`${imgBaseURL}f_webp&s-400x600/tmdb/${person?.profile_path}`}
-          width="400"
-          height="600"
-          className="aspect-3/4"
-          alt={person?.name + "'s avatar"}
-        />
-        {/* </div> */}
+    <div className="max-w-[1200px] h-dvh mx-auto order-first lg:order-last">
+      <div className="grid grid-cols-[400px_1fr] gap-4 p-4">
+        {/* Person Image */}
+        <div className="block aspect-3/4 bg-[#9ca3af1a]">
+          {person?.profile_path ? (
+            <img
+              width={400}
+              height={600}
+              className="object-cover w-full h-full"
+              src={buildURL(person.profile_path, "400x600")}
+              srcSet={`${buildURL(person.profile_path, "400x600")} 1x, ${buildURL(person.profile_path, "800x1200")} 2x`}
+              alt={person?.name + "'s avatar"}
+              fetchpriority="low"
+            />
+          ) : (
+            <div className="h-full flex items-center justify-center opacity-20">
+              <div className="i-ph:user ma text-4xl"></div>
+            </div>
+          )}
+        </div>
+
+        {/* Person Details */}
         <div className="p-4">
           <h1 className="text-3xl mb-4">{person?.name}</h1>
+          <div className="mb-2">
+            <strong className="opacity-70">{person?.known_for_department}</strong>
+          </div>
           <div className="leading-relaxed opacity-80 whitespace-pre-line">
-            {person?.biography}
+            {person?.biography || "Biography not available."}
           </div>
         </div>
       </div>
